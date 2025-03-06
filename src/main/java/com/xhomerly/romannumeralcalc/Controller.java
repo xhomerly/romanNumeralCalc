404f03e7a71dc1a4b29fce574187ec11a12133b9
xhomerly <jan.lysy@xhomerly.com>
2025-03-06 17:51:36 +0100

    funguje to

--- src/main/java/com/xhomerly/romannumeralcalc/Controller.java
+++ src/main/java/com/xhomerly/romannumeralcalc/Controller.java
@@ -8,10 +8,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class Controller {
-    @FXML private TextField romanInput1;
-    @FXML private TextField romanInput2;
-    @FXML private Button plus, minus, multiply, divide;
-    @FXML private Label output;
+    @FXML
+    private TextField romanInput1;
+    @FXML
+    private TextField romanInput2;
+    @FXML
+    private Button plus, minus, multiply, divide;
+    @FXML
+    private Label output;
 
     public void initialize() {
         plus.setOnAction(calculate("+"));
@@ -22,136 +26,146 @@ public class Controller {
 
     public EventHandler<ActionEvent> calculate(String operator) {
         return event -> {
-            String input1 = romanInput1.getText();
-            int input1Int = Integer.parseInt(input1);
-            String input2 = romanInput2.getText();
-            int input2Int = Integer.parseInt(input2);
-            double result = 0;
-            switch (operator) {
-                case "+":
-                    result = input1Int + input2Int;
-                    break;
-                case "-":
-                    result = input1Int - input2Int;
-                    break;
-                case "*":
-                    result = input1Int * input2Int;
-                    break;
-                case "/":
-                    if (input2Int == 0) {
-                        output.setText("Error: Division by zero");
-                        return;
+            if (romanInput1.getText().matches("[I;V;X;L;C;D;M]")&& romanInput2.getText().matches("[I;V;X;L;C;D;M]")){
+                String input1 = romanInput1.getText();
+                int input1Int = romanToDecimal(input1);
+                String input2 = romanInput2.getText();
+                int input2Int = romanToDecimal(input2);
+                double result = 0;
+                switch (operator) {
+                    case "+" -> result = input1Int + input2Int;
+                    case "-" -> result = input1Int - input2Int;
+                    case "*" -> result = input1Int * input2Int;
+                    case "/" -> {
+                        if (input2Int == 0) {
+                            output.setText("Error: Division by zero");
+                            return;
+                        }
+                        result = (double) input1Int / input2Int;
                     }
-                    result = (double) input1Int / input2Int;
-                    break;
+                }
+                System.out.println(result);
+                output.setText(decimalToRoman((int) result));
+            } else {
+                output.setText("Error: Invalid Roman Number");
             }
-            output.setText(""+result);
         };
     }
 
-//    TODO: Romčův kódík
+    public int value(char input) {
+        switch (input) {
+            case 'I' -> {
+                return 1;
+            }
+            case 'V' -> {
+                return 5;
+            }
+            case 'X' -> {
+                return 10;
+            }
+            case 'L' -> {
+                return 50;
+            }
+            case 'C' -> {
+                return 100;
+            }
+            case 'D' -> {
+                return 500;
+            }
+            case 'M' -> {
+                return 1000;
+            }
+        }
+        return -1;
+    }
 
-//    public class HelloController {
-//
-//        @FXML
-//        private Button deleno_B;
-//
-//        @FXML
-//        private TextField firstNumber_TF;
-//
-//        @FXML
-//        private Button krat_B;
-//
-//        @FXML
-//        private Button minus_B;
-//
-//        @FXML
-//        private Button plus_B;
-//
-//        @FXML
-//        private TextField secondNumber_TF;
-//
-//        @FXML
-//        private Label vysledek_L;
-//
-//        @FXML
-//        void calc(ActionEvent event) {
-//            System.out.println(event.getSource());
-//            if (!firstNumber_TF.getText().equals("") && !secondNumber_TF.getText().equals("")) {
-//                int tmp = convert_Roman_To_Int(firstNumber_TF.getText());
-//                int tmp2 = convert_Roman_To_Int(secondNumber_TF.getText());
-//                System.out.println("První: " + tmp + "| Druhe: " + tmp2);
-//                if (event.getSource() == plus_B) {
-//                    vysledek_L.setText(convert_Int_To_Roman(tmp + tmp2));
-//                    System.out.println(tmp + tmp2);
-//                }
-//                else if (event.getSource() == minus_B) {
-//                    vysledek_L.setText(convert_Int_To_Roman(tmp - tmp2));
-//                    System.out.println(tmp - tmp2);
-//                }
-//                else if (event.getSource() == krat_B) {
-//                    vysledek_L.setText(convert_Int_To_Roman(tmp * tmp2));
-//                    System.out.println(tmp * tmp2);
-//                }
-//                else if (event.getSource() == deleno_B) {
-//                    vysledek_L.setText(convert_Int_To_Roman(tmp / tmp2));
-//                    System.out.println(tmp / tmp2);
-//                }
-//            }
-//        }
-//
-//        public int convert_Roman_To_Int(String rn) {
-//            int romanAsInt = 0;
-//            int x = 0;
-//
-//            while (x < rn.length()) {
-//                int currentValue = getRomanValue(rn.charAt(x));
-//
-//                if (x + 1 < rn.length()) {
-//                    int nextValue = getRomanValue(rn.charAt(x + 1));
-//                    if (currentValue < nextValue) {
-//                        romanAsInt -= currentValue;
-//                    } else {
-//                        romanAsInt += currentValue;
-//                    }
-//                } else {
-//                    romanAsInt += currentValue;
-//                }
-//                x++;
-//            }
-//
-//            return romanAsInt;
-//        }
-//
-//        private int getRomanValue(char romanChar) {
-//            switch (romanChar) {
-//                case 'M': return 1000;
-//                case 'D': return 500;
-//                case 'C': return 100;
-//                case 'L': return 50;
-//                case 'X': return 10;
-//                case 'V': return 5;
-//                case 'I': return 1;
-//                default: throw new NumberFormatException("Illegal character in Roman numeral");
-//            }
-//        }
-//
-//
-//        public String convert_Int_To_Roman(int n) {
-//            String roman = "";
-//
-//            int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-//            String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-//
-//            for (int i = 0; i < values.length; i++) {
-//                while (n >= values[i]) {
-//                    roman += symbols[i];
-//                    n -= values[i];
-//                }
-//            }
-//
-//            return roman;
-//        }
-//
-//    }
+    public int romanToDecimal(String str) {
+        int result = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            // Getting value of symbol s[i]
+            int s1 = value(str.charAt(i));
+
+            // Getting value of symbol s[i+1]
+            if (i + 1 < str.length()) {
+                int s2 = value(str.charAt(i + 1));
+
+                // Comparing both values
+                if (s1 >= s2) {
+                    // Value of current symbol
+                    // is greater or equalto
+                    // the next symbol
+                    result = result + s1;
+                } else {
+                    // Value of current symbol is
+                    // less than the next symbol
+                    result = result + s2 - s1;
+                    i++;
+                }
+            } else {
+                result = result + s1;
+            }
+        }
+
+        return result;
+    }
+
+    public String decimalToRoman(int input) {
+        if (input < 1 || input > 3000)
+            return "Invalid Roman Number Value";
+        StringBuilder s = new StringBuilder();
+        while (input >= 1000) {
+            s.append("M");
+            input -= 1000;
+        }
+        while (input >= 900) {
+            s.append("CM");
+            input -= 900;
+        }
+        while (input >= 500) {
+            s.append("D");
+            input -= 500;
+        }
+        while (input >= 400) {
+            s.append("CD");
+            input -= 400;
+        }
+        while (input >= 100) {
+            s.append("C");
+            input -= 100;
+        }
+        while (input >= 90) {
+            s.append("XC");
+            input -= 90;
+        }
+        while (input >= 50) {
+            s.append("L");
+            input -= 50;
+        }
+        while (input >= 40) {
+            s.append("XL");
+            input -= 40;
+        }
+        while (input >= 10) {
+            s.append("X");
+            input -= 10;
+        }
+        while (input >= 9) {
+            s.append("IX");
+            input -= 9;
+        }
+        while (input >= 5) {
+            s.append("V");
+            input -= 5;
+        }
+        while (input >= 4) {
+            s.append("IV");
+            input -= 4;
+        }
+        while (input >= 1) {
+            s.append("I");
+            input -= 1;
+        }
+        return s.toString();
+    }
 }
